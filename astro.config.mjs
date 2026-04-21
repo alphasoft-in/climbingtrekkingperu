c479a9ea42989c7950fb64bb7b735ff3c03d2638
Alphasoft <alphasoftinnovation@gmail.com>
2026-04-21 17:39:43 -0500

    feat: Implementacion de estetica Elite Technical Platinum y optimizacion de itinerarios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://franpiscoadventure.com',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
